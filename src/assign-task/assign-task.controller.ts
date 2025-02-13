27b99415d3d66e437f816e136a4861520d1ab46f
unknown <emranitmbstu@gmail.com>
2025-02-13 18:52:24 +0600

    Assign task module

--- src/assign-task/assign-task.controller.ts
+++ src/assign-task/assign-task.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AssignTaskService } from './assign-task.service';
+import { PermissionGuard } from '../guards/permission.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('assign-task')
+@UseGuards(JwtAuthGuard, PermissionGuard)
 export class AssignTaskController {
   constructor(private readonly assignTaskService: AssignTaskService) {}
+
+  @Get()
+  getTasks(@Req() req) {
+    return this.assignTaskService.getAssignTask(req.user);
+  }
 }
--- src/assign-task/assign-task.service.ts
+++ src/assign-task/assign-task.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from '../interface/user.interface';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AssignTaskService {}
+export class AssignTaskService {
+  constructor(private prisma: PrismaService) {}
+
+  getAssignTask(user: User) {
+    try {
+      return this.prisma.assignTask.findMany({
+        where: { assignUser: user.id },
+        include: {
+          todo: true,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Unable to get assigned task',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
