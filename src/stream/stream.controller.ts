f42f5b74c6f8104c5b7be20eb33772cc620c915c
MD Emran Ali <emranitmbstu@gmail.com>
2025-03-10 16:14:51 +0600

    User Authentication in stream

--- src/stream/stream.controller.ts
+++ src/stream/stream.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { StreamService } from './stream.service';
+import { GetUser } from '../decorators/get-user.decorator';
+import { User } from '../interface/user.interface';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PermissionGuard } from '../guards/permission.guard';
 
 @Controller('stream')
-export class StreamController {}
+@UseGuards(JwtAuthGuard, PermissionGuard)
+export class StreamController {
+  constructor(private readonly streamService: StreamService) {}
+  @Post('/create-channel')
+  createChannel(@GetUser() user: User) {
+    return this.streamService.createChannel('newchannelid', `${user.id}`);
+  }
+  @Post('send/:userId')
+  sendMessage(@Param('userId') userId: string) {
+    return this.streamService.sendMessage(
+      'newchannelid',
+      userId,
+      'Hello test Massage',
+    );
+  }
+}
